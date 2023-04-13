eb10859372fb20f7f59e16fdd30ebb6de42f1315
Gurleen Kaur <gurleen.kaur101010@gmail.com>
2023-04-13 16:16:44 -0400

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Gurleen Kaur on 2023-04-13.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
